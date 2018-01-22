3db1e7bf0ec169a4dafc581430a8e45a5b50219d
Matthew Reishus <Matthew.Reishus@experian.com>
2018-01-22 08:13:23 -0600

    Simple react table rendering

--- src/TableTest.js
+++ src/TableTest.js
@@ -1,10 +1,69 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import ReactTable from "react-table";
+import "react-table/react-table.css";
+import { makeData } from "./utils";
 
 class TableTest extends Component {
+  constructor() {
+    super();
+    this.state = {
+      tableData: makeData()
+    };
+  }
+
+  getColumns() {
+    return [
+      {
+        Header: "Name",
+        columns: [
+          {
+            Header: "First Name",
+            accessor: "firstName"
+          },
+          {
+            Header: "Last Name",
+            id: "lastName",
+            accessor: d => d.lastName
+          }
+        ]
+      },
+      {
+        Header: "Info",
+        columns: [
+          {
+            Header: "Age",
+            accessor: "age"
+          },
+          {
+            Header: "Status",
+            accessor: "status"
+          }
+        ]
+      },
+      {
+        Header: "Stats",
+        columns: [
+          {
+            Header: "Visits",
+            accessor: "visits"
+          }
+        ]
+      }
+    ];
+  }
+
   render() {
+    const { tableData } = this.state;
+    console.log(tableData);
     return (
       <div>
         This is tabletest
+        <ReactTable
+          data={tableData}
+          columns={this.getColumns()}
+          defaultPageSize={10}
+          className="-striped -highlight"
+        />
       </div>
     );
   }
