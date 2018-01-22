5318722f9b9b8b3c1518ada942a55f1dbc8053fb
Matthew Reishus <Matthew.Reishus@experian.com>
2018-01-22 07:57:41 -0600

    Install prettier and add TableTest component

---
+++ src/TableTest.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TableTest extends Component {
+  render() {
+    return (
+      <div>
+        This is tabletest
+      </div>
+    );
+  }
+}
+
+export default TableTest;
